5366785fca964ef24c27d82f9953236b7e138b0a
Manel Ballesteros <ballesterosoriol@gmail.com>
2018-07-03 09:46:05 +0200

    Simple API call - focused on improving coding style.

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,6 @@ document.getElementById("searchBtn").addEventListener("click", function () {
     
 });
 
-
 function getUserName() {
     var userSearch = document.getElementById("searchBar").value;
 
@@ -18,7 +17,6 @@ function getUserName() {
     return userSearch;
 
 };
-
 
 function createImg(archive, descrip) {
     var img = document.createElement("img");
@@ -29,7 +27,6 @@ function createImg(archive, descrip) {
     return img;
 
 }
-
 
 function fetchCompleted(url, successHandler, error) {
 
@@ -50,75 +47,6 @@ function fetchCompleted(url, successHandler, error) {
         .catch(error);
 };
 
-
-
-
-//////make a call to user info API
-//function fetchUser(user) {
-//
-//    fetch("https://api.github.com/users/" + user, {
-//
-//        method: "GET",
-//
-//    }).then(function (response) {
-//
-//        if (response.ok) {
-//            return response.json();
-//        }
-//
-//        throw new Error(response.statusText);
-//
-//    }).then(function (json) {
-//
-//        console.log(json);
-//
-//        //hide and show what it is required
-//        document.getElementById("alertContainer").style.display = "none";
-//        document.getElementById("user").style.display = "block";
-//
-//        //print info about user
-//        bioUser(data);
-//
-//
-//    }).catch(function (error) {
-//        console.log("Request failed: " + error.message);
-//
-//        //        //show the alert div and hide the info about user (no user, no info)
-//        //        document.getElementById("alertContainer").style.display = "flex";
-//        //        document.getElementById("alert").style.display = "block";
-//        //        document.getElementById("user").style.display = "none";
-//        errorUser();
-//
-//    });
-//};
-//
-//
-////make a call to user repo info 
-//function fetchRepos(user) {
-//
-//    fetch("https://api.github.com/users/" + user + "/repos", {
-//
-//            method: "GET",
-//
-//        }).then(function (response) {
-//
-//            if (response.ok) {
-//                return response.json();
-//            }
-//
-//            throw new Error(response.statusText);
-//
-//        })
-//        .then(userRepos)
-//        .catch(function (error) {
-//            console.log("Request failed: " + error.message);
-//
-//        });
-//};
-
-
-
-//take and print main info of user's bio
 function bioUser(data) {
     var avatar = document.getElementById("avatar");
     var nickName = document.getElementById("nickName");
@@ -138,41 +66,15 @@ function bioUser(data) {
    
     nickName.appendChild(document.createTextNode("@" + dataNickName));
 
-
-//    if (dataFullName != null) {
-//        fullName.appendChild(document.createTextNode(dataFullName));
-//        
-//    } else {
-//        fullName.appendChild(document.createTextNode("No name saved"));
-//        
-//    }
-//  Cuando tenemos un escenario con tres situaciones una línia muy elegante es el uso de TERNARY; ternary tiene la sintaxi: 
-//  "var z = (condicional) ? x : y" --> funcionamiento = a la var z, si la condicion se cumple, aplicale X, sino, aplicale Y. 
-// Ejemplo: aquí debajo: a fullName le daremos un texto = "fullName.textContent"; (dataFullName es Null)? sí-aplicale "No name saved" : no-aplicale dataFullName;
     fullName.textContent = (dataFullName == null) ? "No name saved" : dataFullName;
     
-    
-//    if (dataBio != null) {
-//        bio.appendChild(document.createTextNode(dataBio));
-//        
-//    } else {
-//        bio.appendChild(document.createTextNode("No bio saved"));
-//        
-//    }
-//  Otro caso igual de ternary
     bio.textContent = (dataBio == null) ? "No bio saved" : dataBio; 
 
-
-    //Es mejor praxis no aplicar CSS des de el JS. Mejor seria asignar una o otra clase y que sea la clase la que contempla el estilo deseado. 
     document.getElementById("alertContainer").style.display = "none";
     document.getElementById("user").style.display = "block";
 
-
 }
 
-
-//print info about repos of the user. Print the name of the repo directly in this function; 
-//call the functions to print Stars and Forks
 function userRepos(data) {
     console.log(data);
     
@@ -200,8 +102,6 @@ function userRepos(data) {
     }
 }
 
-
-//print star image and number of it of the singular repo
 function repoStars(repo, div) {
     var numberStars = repo.stargazers_count;
 
@@ -212,8 +112,6 @@ function repoStars(repo, div) {
 
 }
 
-
-//print fork image and number of it of this singular repo
 function repoForks(repo, div) {
     var numberForks = repo.forks;
 
@@ -224,36 +122,8 @@ function repoForks(repo, div) {
 
 }
 
-
-//print an error message if there is no result from the search
 function errorUser() {
     document.getElementById("alertContainer").style.display = "flex";
     document.getElementById("user").style.display="none";
     
-//  Si el text no canvia mai no cal que el fem a través de DOM, ho deixem ja preparat al HTML.
-//    var alert = document.getElementById("alert");
-//    alert.textContent="Does not exist";
-
 }
-
-
-//ANOTACIONES SOBRE TERNARY
-//var name="A";
-//    var count;
-//    if (name == "A") {
-//        count = 1;
-//    } else {
-//        count = 3;
-//    }
-//    
-//    function ternary(a, b, c){
-//        if (a){ 
-//            return b;
-//        }else{
-//            return c;
-//        }
-//    }
-//        
-//    a ? b : c;
-//    count = (name == "A") ? 1 : 3;
-//    count = ternary(name == "A", 1, 3);
